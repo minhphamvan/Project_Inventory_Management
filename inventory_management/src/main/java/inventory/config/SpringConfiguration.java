887c5c6dd6d5a0aa4c27a4bbd52d96ceafb74b20
minhpham <mnhphm136@gmail.com>
2022-03-05 15:10:17 +0700

    ok

---
+++ inventory_management/src/main/java/inventory/config/SpringConfiguration.java
@@ -0,0 +1,29 @@
+package inventory.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc // mvc:annotation-driven
+@ComponentScan("inventory") // trỏ đến packet chứa các Controller
+public class SpringConfiguration extends WebMvcConfigurerAdapter {
+	@Bean
+	public InternalResourceViewResolver viewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+
+		viewResolver.setPrefix("/WEB-INF/views/");
+		viewResolver.setSuffix(".jsp");
+
+		return viewResolver;
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/static/**").addResourceLocations("/static/");
+	}
+}
